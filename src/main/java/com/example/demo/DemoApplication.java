291bc1d9b9b453e639b913d1c4b7c4ec0d7c2e5d
AntA585 <allen.1295@osu.edu>
2021-03-06 10:18:54 -0500

    Section-2, creating customer layers

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,29 +13,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	Customer getCustomer(){
-		return new Customer(1L,"James Bond");
 
-	}
 
-	class Customer{
-		private final Long id;
-		private final String name;
 
-		Customer(Long id, String name){
-
-			this.id = id;
-			this.name = name;
-		}
-
-		public Long getId() {
-			return id;
-		}
-
-		public String getName() {
-			return name;
-		}
-	}
 
 }
