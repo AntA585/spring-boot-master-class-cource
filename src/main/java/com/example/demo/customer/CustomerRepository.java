7420d43559fa504752c4528eb9e9839ea041ce6c
AntA585 <allen.1295@osu.edu>
2021-03-06 11:11:21 -0500

    Section-3, connecting all the layers for the customer object

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -1,4 +1,19 @@
 package com.example.demo.customer;
 
-public class CustomerRepository {
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@Primary
+public class CustomerRepository implements CustomerRepo{
+    public List<Customer> getCustomers() {
+        //TODO connect to real DB
+        return Collections.singletonList(
+                new Customer(1L,"TODO.Implement Real DB")
+        );
+    }
 }
