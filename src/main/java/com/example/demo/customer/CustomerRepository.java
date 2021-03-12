16fa1cfb0260a1cf1ecc7007c5f9b4e2976566ce
AntA585 <allen.1295@osu.edu>
2021-03-11 22:47:44 -0500

    Added configurations to instantiate the customer repository object

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -2,14 +2,14 @@ package com.example.demo.customer;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Component
-@Primary
 public class CustomerRepository implements CustomerRepo{
+    @Override
     public List<Customer> getCustomers() {
         //TODO connect to real DB
         return Collections.singletonList(
--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -2,19 +2,20 @@ package com.example.demo.customer;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Component
+@Service
 public class CustomerService {
 
     private CustomerRepo customerRepo;
+
     @Autowired
-    public CustomerService(CustomerRepo customerRepo){
+    public CustomerService(CustomerRepo customerRepo) {
         this.customerRepo = customerRepo;
     }
+
     public List<Customer> getCustomer() {
         return customerRepo.getCustomers();
     }
