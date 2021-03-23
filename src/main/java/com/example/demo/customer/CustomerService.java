e2f99d463b4690678bdad344e486d5db672e4760
AntA585 <allen.1295@osu.edu>
2021-03-23 14:58:47 -0400

    Section 5 - Added crud methods and updated service repo to be able to get single customers and all customers. Added new versioning

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -3,6 +3,7 @@ package com.example.demo.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -16,7 +17,15 @@ public class CustomerService {
         this.customerRepo = customerRepo;
     }
 
-    public List<Customer> getCustomer() {
+    public List<Customer> getCustomers() {
         return customerRepo.getCustomers();
+    }
+
+    public Customer getCustomer(Long id) {
+        return getCustomers()
+                .stream()
+                .filter(customer -> customer.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("customer not found"));
     }
 }
