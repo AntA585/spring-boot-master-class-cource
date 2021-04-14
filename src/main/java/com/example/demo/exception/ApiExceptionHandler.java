5d37dfa7309509618870dcae399d444dbe0f938b
AntA585 <allen.1295@osu.edu>
2021-04-14 15:36:56 -0400

    Added logging to the Customer and ApiEcxeptionHandler classes. Added a logging configuration to the application.properties file

--- src/main/java/com/example/demo/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/demo/exception/ApiExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.demo.exception;
 
 import ch.qos.logback.core.net.SocketConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,9 @@ import java.time.ZonedDateTime;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
     @ExceptionHandler(value = ApiRequestException.class)
     public ResponseEntity<Object> handleApiRequestException(ApiRequestException e){
 
