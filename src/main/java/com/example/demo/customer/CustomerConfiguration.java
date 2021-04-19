f5c31a0f4f49f11446f6219db5bfb6d28a6ecbf2
AntA585 <allen.1295@osu.edu>
2021-04-18 22:38:34 -0400

    Accessing properties from the properties file using Value and environment. Mapped configuration to a class. Spring profiles

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,9 +1,12 @@
 package com.example.demo.customer;
 
+import com.example.demo.infoapp.InfoApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
@@ -11,10 +14,19 @@ public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepository:false}")
     private Boolean useFakeCustomerRepository;
 
+    @Value("${info.app.name}")
+    private String appName;
+
+    @Autowired
+    private Environment environment;
+
     @Bean
-    CommandLineRunner commandLineRunner(){
+    CommandLineRunner commandLineRunner(InfoApp infoApp){
         return args -> {
             System.out.println("Command Line Runner horray");
+            System.out.println(appName);
+            System.out.println(environment.getProperty("info.app.description"));
+            System.out.println(infoApp);
         };
     }
 
